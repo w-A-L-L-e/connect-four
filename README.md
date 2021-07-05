e302c91ac1c905c412882bbdb197b8af496e71b1
Walter Schreppers <schreppers@gmail.com>
2021-07-05 13:21:08 +0200

    Connect four written on a rainy sunday.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # connect-four
 A Pure JavaScript grid-based game
+
+Connect Four (also known as Four Up, Plot Four, Find Four, Four in a Row, Four in a Line, Drop Four, and Gravitrips (in Soviet Union)) is a two-player board game in which the players first choose a color and then take turns dropping one colored disc from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs.
+
+
+## Rules of Connect Four
+* You will start as player One (red) or Player Two (yellow). This is shown in the status.
+* You will take your turn by clicking a column and thereby placing a disk in that column that drops down. It is placed automatically above any existing disks.
+* Once your go is taken and you did not win, It will be the other players turn.
+* The gamer will continue until a winning combination of four is reached by player One or player Two, or until the grid is full (then it's a tie game and this is also shown).
+
+## Why it was created.
+Did some random browsing and saw a video from Ania Kubow about creating connect four in javascript.
+But her static array gamechecking routine looks like the wrong approach. Also the gui and not being able to click anywhere in a column annoyed me and my son when
+we tried to play it.
+
+Wrote my own version from scratch in a couple of hours. And played it with my sun Noah Schreppers. He suggested adding specials and animations etc.
+Added some of them like animating a win and a tie nicely. Might add some more of those in the near future ;).
+
+The checkPlayerWins is the meat of the algorithm (also it can be further shortened now, I added 8 directions but during playing we noticed we had to cover an edgecase in checkDirection. Fixed it by first walking to end and then doing the direction check. This however also allows further slimming down the 8 checks down to 4 just 2 vertical and 2 diagonal, might do this when I have time). For now this version however already fully works and is pretty nice to play with.
+
---
+++ connect4.js
@@ -0,0 +1,255 @@
+let ROWS = 6
+let COLS = 7
+let currentPlayer = 1
+let gameOver = false
+let winAnimating = false;
+
+let bs = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+]
+
+
+function setStatus(msg){
+  statusTxt = document.getElementById("status")
+  statusTxt.innerText = msg;
+}
+
+function setCellClass(row, col, newclass){
+  let cell = document.getElementById(row+"_"+col)
+  cell.className = newclass;
+}
+
+function showCurrentPlayer(){
+  playerWidget = document.getElementById("current_player")
+  playerWidget.className = "cell player"+currentPlayer
+  playerWidget.firstElementChild.innerText = "Player "+currentPlayer
+}
+
+function resetGame(){
+  for(var i=0;i<ROWS;i++){
+    for(var j=0;j<COLS;j++){
+      bs[i][j]=0;
+      currentPlayer=1;
+      setCellClass(i, j, "cell")
+      setStatus("");
+    }
+  }
+
+  gameOver=false;
+  winAnimating=false;
+  currentPlayer = Math.ceil(Math.random()*2)
+  showCurrentPlayer()
+}
+
+function blinkWin(player, row, col, dx, dy){
+  // turn winning cells off
+  setTimeout(()=>{
+    let r=row, c=col;
+    for(let i=0;i<4;i++){
+      setCellClass(r, c, "cell")
+      r+=dx; c+=dy;
+    }
+  }, 400)
+
+  //turn winning cells on
+  r=row, c=col;
+  setTimeout(()=>{
+    let r=row, c=col;
+    for(let i=0;i<4;i++){
+      setCellClass(r, c, "cell player"+player)
+      r+=dx; c+=dy;
+    }
+  }, 800)
+}
+
+function blinkEntireBoard(){
+  //clear board
+  setTimeout(()=>{
+    for(let i=0; i<ROWS; i++){
+      for(let j=0; j<COLS; j++){
+        setCellClass(i,j, "cell")
+      }
+    } 
+  }, 400);
+
+
+  //set board back to boardstate bs
+  setTimeout(()=>{
+    for(let i=0; i<ROWS; i++){
+      for(let j=0; j<COLS; j++){
+
+        if(bs[i][j]>0) setCellClass(i, j, "cell player"+bs[i][j] )
+      }
+    } 
+  }, 800);
+}
+
+
+function checkDirection(player, start_row, start_col, dx, dy){
+  let count=0;
+  let row = start_row;
+  let col = start_col;
+
+  // first walk in opposite direction to beginning of a string
+  let prow, pcol;
+  while(true){
+    prow=row;
+    pcol=col;
+
+    row-=dx;
+    col-=dy;
+
+    // reach different color, or out of bounds, break
+    if((col<0)||(row<0)) break;
+    if((col>=COLS)||(row>=ROWS)) break;
+    if( bs[row][col] != player ) break;
+  }
+
+  // get last good pos at beginning of string
+  row=prow;
+  col=pcol;
+
+  // now see if we have 4 in a row, exit if we hit wall or go out of bounds
+  for(count=1;count<4;count++){
+    row += dx;
+    col += dy;
+
+    //check bounds
+    if((col<0)||(row<0)) return false;
+    if((col>=COLS)||(row>=ROWS)) return false;
+     
+    //check same color as player color
+    if( bs[row][col]!=player ) return false;
+  }
+
+  // we've found 4 successive cells with player color in a row
+  // blink the winning string
+  winAnimating = true;
+  setTimeout( ()=>{blinkWin(player, prow, pcol, dx, dy)}, 10 )
+  setTimeout( ()=>{blinkWin(player, prow, pcol, dx, dy)}, 1000 )
+  setTimeout( ()=>{blinkWin(player, prow, pcol, dx, dy)}, 2000 )
+  setTimeout( ()=>{winAnimating=false}, 3000 ); // after 4 seconds animation is done
+
+  return true;
+}
+
+function checkPlayerWins(player, row, col){
+  if( checkDirection(player, row, col, 0, -1) ) return true; // up win
+  if( checkDirection(player, row, col, 0,  1) ) return true; // down win
+
+  if( checkDirection(player, row, col, -1, 0) ) return true; // left win
+  if( checkDirection(player, row, col, 1, 0 ) ) return true; // right win
+
+  if( checkDirection(player, row, col, 1, -1) ) return true; // up right win
+  if( checkDirection(player, row, col, 1,  1) ) return true; // down right win
+
+  if( checkDirection(player, row, col, -1, -1) ) return true; // up left win
+  if( checkDirection(player, row, col, -1, 1 ) ) return true; // down left win
+
+  return false; // no winning direction found
+}
+
+
+function checkFullBoard(){
+  for(var i=0;i<ROWS;i++){
+    for(var j=0;j<COLS;j++){
+      if( bs[i][j]==0 ) return false;
+    }
+  }
+
+  return true;
+}
+
+
+function clickColumn(col){
+  console.log("column clicked=", col)
+  if(winAnimating){
+    console.log("ignoring clicks!")
+    return; //ignore clicks until animation is done
+  }
+
+  if(gameOver){ 
+    resetGame();
+    return; //stop clicks with game over state
+  }
+
+  let foundEmpty=false;
+  for(let row=5;row>=0;row--){
+    if(bs[row][col]==0){
+      bs[row][col] = currentPlayer;
+      setCellClass(row, col, "cell player"+currentPlayer)
+      foundEmpty = true;
+      gameOver = checkPlayerWins(currentPlayer, row, col)
+      break;
+    }
+  }
+
+  if(!foundEmpty){
+    console.log("user clicked full row, let him try again!")
+    return
+  }
+
+  if(gameOver){
+    setStatus("Player " + currentPlayer + " wins!");
+    return;
+  }
+
+  if(checkFullBoard()){
+    setStatus("It's a tie! You are both winners ;)")
+    gameOver = true;
+    winAnimating = true;
+    
+    setTimeout(()=>{blinkEntireBoard()}, 500);
+    setTimeout(()=>{blinkEntireBoard()}, 1500);
+    setTimeout(()=>{blinkEntireBoard()}, 2500);
+
+    setTimeout(()=>{winAnimating=false}, 3000);
+  }
+
+  //switch player
+  if( currentPlayer == 1 )
+    currentPlayer = 2
+  else
+    currentPlayer = 1
+
+  showCurrentPlayer()
+}
+
+function addCol(board, col_index){
+  let col = document.createElement("div")
+  col.classList.add("column")
+  board.appendChild(col)
+  col.onclick = ()=>{clickColumn(col_index)}
+  return col
+}
+
+function addCell(coldiv, row, col){
+  let cell = document.createElement("div")
+  cell.className = "cell"
+  cell.id = row + "_" + col
+  coldiv.appendChild(cell)
+}
+
+function create_board(){
+  let game = document.getElementById("game")
+  let board = document.createElement("div")
+  board.classList.add("board")
+  game.appendChild(board)
+
+  for(let i=0;i<COLS;i++){
+    let coldiv = addCol(board, i)
+    for(let j=0;j<ROWS;j++){
+      addCell(coldiv, j, i)
+    }
+  }
+  resetGame();
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  create_board()
+})
